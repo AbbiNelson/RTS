d295d76ac0f7a534232d5104158b4baab3fe48cf
MUKILTEO\1630107 <160107@mukilteo.wednet.edu>
2025-02-19 09:35:17 -0800

    Before Unity 6

--- 3D Template/Assets/UnitFollowState.cs
+++ 3D Template/Assets/UnitFollowState.cs
@@ -1,24 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UnitFollowState : StateMachineBehaviour
 {
+    AttackController attackController;
+
+    NavMeshAgent agent;
+
+    public float attackingDistance = 1f;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
-
+        attackController = animator.transform.GetComponent<AttackController>();
+        agent = animator.transform.GetComponent<NavMeshAgent>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
     {
+        if (attackController.targetToAttack == null)
+        {
+            animator.SetBool("isFollow", false);
+        }
 
+        agent.SetDestination(attackController.targetToAttack.position);
+        animator.transform.LookAt(attackController.targetToAttack);
+
+        float distanceFromTarget = Vector3.Distance(attackController.targetToAttack.position, animator.transform.position);
+        if (distanceFromTarget < attackingDistance)
+        {
+            animator.SetBool("isAttack", true);
+        }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateinfo, int layerindex)
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        agent.SetDestination(animator.transform.position);
     }
 }
